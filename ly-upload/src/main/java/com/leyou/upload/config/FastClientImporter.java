dbb94c528bc0227388b54b6c45db8a941e64391e
felixj <1045985453@qq.com>
2020-03-04 10:34:29 +0800

    上传图片全部完成

--- ly-upload/src/main/java/com/leyou/upload/config/FastClientImporter.java
+++ ly-upload/src/main/java/com/leyou/upload/config/FastClientImporter.java
@@ -1,6 +1,10 @@
 package com.leyou.upload.config;
 
+import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
 
 /**
  * @Author Felix
@@ -8,5 +12,8 @@ import org.springframework.context.annotation.Configuration;
  * 面向对象面向卿，不负代码不负君
  */
 @Configuration
+@Import(FdfsClientConfig.class)
+//解决JVM重复注册bean的问题
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 public class FastClientImporter {
 }
--- ly-upload/src/main/java/com/leyou/upload/service/impl/UploadServiceImpl.java
+++ ly-upload/src/main/java/com/leyou/upload/service/impl/UploadServiceImpl.java
@@ -1,9 +1,15 @@
 package com.leyou.upload.service.impl;
 
+import com.github.tobato.fastdfs.domain.fdfs.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.leyou.common.enums.ExceptionEnum;
 import com.leyou.common.exception.LyException;
+import com.leyou.upload.config.UploadProperties;
 import com.leyou.upload.service.UploadService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,10 +27,16 @@ import java.util.List;
  */
 @Service
 @Slf4j
+@EnableConfigurationProperties(UploadProperties.class)
 public class UploadServiceImpl implements UploadService {
 
-    private static final List<String> ALLOW_TYPE = Arrays.asList("image/png", "image/jpeg", "image/bmp");
+    //private static final List<String> ALLOW_TYPE = Arrays.asList("image/png", "image/jpeg", "image/jpg", "image/bmp");
 
+    @Autowired
+    private FastFileStorageClient fastFileStorageClient;
+
+    @Autowired
+    private UploadProperties uploadProperties;
     /**
      * @description: 上传图片
      * @auther: Felix
@@ -34,7 +46,7 @@ public class UploadServiceImpl implements UploadService {
         try {
             //检验文件类型
             String contentType = file.getContentType();
-            if (!ALLOW_TYPE.contains(contentType)) {
+            if (!uploadProperties.getAllowTypes().contains(contentType)) {
                 throw new LyException(ExceptionEnum.INVALID_FILE_TYPE);
             }
             //检验文件内容
@@ -42,12 +54,15 @@ public class UploadServiceImpl implements UploadService {
             if (null == image) {
                 throw new LyException(ExceptionEnum.INVALID_FILE_TYPE);
             }
-            //目标路径
+           /* //目标路径
             File dest = new File("D:/UploadFile", file.getOriginalFilename());
             //保存文件
-            file.transferTo(dest);
+            file.transferTo(dest);*/
+            //上传到fdfs
+            String extension = StringUtils.substringAfterLast(file.getOriginalFilename(), ".");
+            StorePath storePath = fastFileStorageClient.uploadFile(file.getInputStream(), file.getSize(), extension, null);
             //返回路径
-            return "http://" + file.getOriginalFilename();
+            return uploadProperties.getBaseUrl() + storePath.getFullPath();
         } catch (IOException e) {
             log.error("文件上传失败", e);
             throw new LyException(ExceptionEnum.UPLOAD_FILE_FAIL);
