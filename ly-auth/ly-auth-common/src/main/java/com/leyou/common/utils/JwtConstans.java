faec68dba06d01caf72b1642e0d464bfdd8a88e9
FelixJ27 <1045985453@qq.com>
2021-07-09 18:15:44 +0800

    授权微服务

---
+++ ly-auth/ly-auth-common/src/main/java/com/leyou/common/utils/JwtConstans.java
@@ -0,0 +1,10 @@
+package com.leyou.common.utils;
+
+/**
+ * @Author: Felix
+ * @Description:
+ */
+public abstract class JwtConstans {
+    public static final String JWT_KEY_ID = "id";
+    public static final String JWT_KEY_USER_NAME = "username";
+}
