c28f5ea50c17c691f40d2a48a5be0a5796b9d679
felixj27 <1045985453@qq.com>
2021-11-30 19:48:23 +0800

    test commit

--- ly-page/src/main/java/com/leyou/page/web/PageController.java
+++ ly-page/src/main/java/com/leyou/page/web/PageController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,7 +15,7 @@ import java.util.Map;
  * @Author: Felix
  * @Description:
  */
-@Controller
+@RestController
 public class PageController {
 
     @Autowired
